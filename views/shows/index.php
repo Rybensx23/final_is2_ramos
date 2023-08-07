5e3f81c99960eb5f012fe8c47c7e6a707763ad31
Rybensx <anderson_arf@hotmail.es>
2023-08-07 15:24:48 -0600

    modificando estilos

--- views/shows/index.php
+++ views/shows/index.php
@@ -1,30 +1,36 @@
 <div class="row justify-content-center">
     <div class="col-lg-10">
         <?php foreach ($empleadosPorAreas as $area => $empleados) : ?>
-            <h2><?= $area ?></h2>
             <table class="table table-bordered table-hover">
+                <thead class="table-dark">
+                    <tr>
+                        <th colspan="7" class="text-center"><?= $area ?></th>
+                    </tr>
+                </thead>
                 <thead class="table-warning">
                     <tr>
-                    <th>No.</th>
-                    <th>Nombre</th>
-                    <th>DPI</th>
-                    <th>Puesto</th>
-                    <th>Edad</th>
-                    <th>Sexo</th>
-                    <th>Sueldo</th>
-                </tr>
-            </thead>
-            <tbody>
+                        <th>No.</th>
+                        <th>Nombre</th>
+                        <th>DPI</th>
+                        <th>Puesto</th>
+                        <th>Edad</th>
+                        <th>Sexo</th>
+                        <th>Sueldo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $isGray = false;?>
                     <?php foreach ($empleados as $indice => $empleado) : ?>
-                        <tr>
+                        <tr class="<?= $isGray ? 'table-secondary' : '' ?>">
                             <td><?= $indice + 1 ?></td>
                             <td><?= $empleado['emp_nom'] ?></td>
                             <td><?= $empleado['emp_dpi'] ?></td>
                             <td><?= $empleado['pue_descr'] ?></td>
                             <td><?= $empleado['emp_edad'] ?></td>
                             <td><?= $empleado['sex_descr'] ?></td>
-                            <td>Q. <?= $empleado['pue_suel'] ?></td>
+                            <td><?= $empleado['pue_suel'] ?></td>
                         </tr>
+                        <?php $isGray = !$isGray;?>
                     <?php endforeach ?>
                 </tbody>
             </table>
